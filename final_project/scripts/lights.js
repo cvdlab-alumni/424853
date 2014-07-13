0de1d042a1eaf64e1855f1fbb98469640096d92c
dordika <dorjan.dika@gmail.com>
2014-07-13 12:01:11 +0200

    intensità light e pendant lamp

--- final_project/scripts/lights.js
+++ final_project/scripts/lights.js
@@ -1,29 +1,29 @@
-var lampMaterial = new THREE.MeshPhongMaterial({ color: 0xB0C4DE , shading: THREE.SmoothShading, shininess: 30, metal: true, side: THREE.DoubleSide});
+var lampMaterial = new THREE.MeshPhongMaterial({ shininess: 50,
+            specular: '#B5FFFA',
+            transparent: true,
+            opacity: 0.0,
+            metal: true,
+            side: THREE.DoubleSide
+});
 
 function putLights (scene,apartment) {
 
 	
 		
-	var dir1 = new THREE.DirectionalLight(0xffffff);
-      // dir1.position.set(50, 100, -50);
-      dir1.position.set(25, 70, 50);
-      dir1.intensity = 0.7; 
-      apartment.add(dir1);
+	var dirLight1 = new THREE.DirectionalLight(0xffffff);
+     
+      dirLight1.position.set(25, 70, 50);
+      dirLight1.intensity = 0.7; 
+      apartment.add(dirLight1);
+     
+      var dirLight2 = new THREE.DirectionalLight(0xffffff);
+      dirLight2.position.set(-30, -70, 10);
+      dirLight2.intensity = 0.7;
+      apartment.add(dirLight2);
 
-      var dir1Helper = new THREE.DirectionalLightHelper(dir1,3);
-      scene.add(dir1Helper)
-
-      var dir2 = new THREE.DirectionalLight(0xffffff);
-      // dir2.position.set(-50, 100, 50);
-      dir2.position.set(-30, -70, 10);
-      dir2.intensity = 0.7;
-      apartment.add(dir2);
-
-      var dir2Helper = new THREE.DirectionalLightHelper(dir2,3);
-      scene.add(dir2Helper)
 
       //Bathroom Light
-      var light1 = mkLamp(0.3, 10);
+      var light1 = mkLamp(10);
       apartment.add(light1);
       light1.position.set(16,32.6,3);
       
@@ -31,21 +31,21 @@ function putLights (scene,apartment) {
       /*
       //Corridor Light
       */
-      var light2 = mkLamp(0.3, 10);
+      var light2 = mkLamp(10);
       apartment.add(light2);
       light2.position.set(6.3,32.6,3)
 
       /*
       //Mainroom Light
       */
-      var light3 = mkLamp(0.3, 30);
+      var light3 = mkLamp(30);
       apartment.add(light3);
       light3.position.set(10.2,16,3)
 
       /*
       //Balcony Light
       */
-      var light4 = mkLamp(0.3, 10);
+      var light4 = mkLamp(10);
       apartment.add(light4);
       light4.position.set(6,3,3)
 
@@ -54,42 +54,36 @@ function putLights (scene,apartment) {
 
 }
 
-function mkLamp(radius, distance){
-            
-            var lampGeometry = new THREE.SphereGeometry(radius, 8, 8, 0, Math.PI, 0, Math.PI);
-                
-            // lampMaterial.side = THREE.DoubleSide;
-            var lamp = new THREE.Mesh(lampGeometry, lampMaterial);
-            lamp.scale.z=0.5;
+function mkLamp(distance){
+      
+      var pendantLampObj = 'models/pendantLamp.obj';
+      var pendantLamp = importObj(pendantLampObj);
+      pendantLamp.scale.set(.5,.7,.5);
+      pendantLamp.rotation.set(Math.PI/2,Math.PI/2,0);
 
-            //   var spotLight = new THREE.SpotLight(0xffffff);
-            //   spotLight.angle=Math.PI/2;
-            //   spotLight.intensity=0;
-            //   lamp.add(spotLight);
-            // lamp.spotLight=spotLight;
+      var lampGeometry = new THREE.SphereGeometry(.2, 8, 8);
+          
+      var lamp = new THREE.Mesh(lampGeometry, lampMaterial);
 
-            var plight = new THREE.PointLight( 0xFFFFFF, 0, distance );
-            lamp.add(plight);
-            lamp.pointLight =plight;
-            // plight.intensity=0;
-            plight.position.set(0,0,-1);
-
-            var t = new THREE.Object3D();
-            lamp.add(t);
-            t.position.set(0,0,-6);
-            lamp.target = t;
-
-            toIntersect.push(lamp);
-            lamp.on=false;
+      var plight = new THREE.PointLight( 0xFFFFFF, 0, distance );
+      lamp.add(plight);
+      lamp.pointLight =plight;
+      plight.position.set(0,0,-1);
+      
+      toIntersect.push(lamp);
+      lamp.on=false;
       lamp.interact=function(){
             if(!this.on){
-                  this.pointLight.intensity=2;
+                  this.pointLight.intensity=.8;
                   this.on=true;
             } else {
                   this.pointLight.intensity=0;
                   this.on=false;
             }
       }
-      return lamp;
+      
+      pendantLamp.add(lamp);
+      lamp.position.y = -.6;
+      return pendantLamp;
 
 }
